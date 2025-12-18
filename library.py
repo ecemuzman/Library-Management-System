b2676441b5f1f55379356f770bef5310bab270cf
Ecem Uzman <148550163+ecemuzman@users.noreply.github.com>
2025-12-18 14:40:37 +0300

    Refactor Library methods and improve file handling

--- library.py
+++ library.py
@@ -1,74 +1,46 @@
 class Library:
-    def __init__(self):
-        self.file = open("books.txt", "a+")
 
-    def listBook(self):
-        self.file.seek(0)
-        books_data = self.file.read()
-        books_list = books_data.splitlines()
-        if not books_list:
+    def list_books(self):
+        with open("books.txt", "a+") as file:
+            file.seek(0)
+            books = file.read().splitlines()
+
+        if not books:
             print("There is no book in the list.")
         else:
-            for book in books_list:
-               print(book)
-        
-    def addBook(self):
+            for book in books:
+                print(book)
+
+    def add_book(self):
         name = input("Book Name: ")
         author = input("Author: ")
         year = input("Release Year: ")
-        pagenumber = input("Pages: ")
+        pages = input("Pages: ")
 
-        with open("books.txt", "a+") as f:
-            f.write(f"{name}, {author}, {year}, {pagenumber}\n")
-    
-    def removeBook (self):
+        with open("books.txt", "a") as file:
+            file.write(f"{name}, {author}, {year}, {pages}\n")
+
+    def remove_book(self):
         name = input("Book Name: ")
-        with open ("books.txt", "r") as f:
-            books = f.readlines()
-        
-        books_list = []
-        for line in books :
-            books_list.append(line.strip())
 
-        index_to_remove = None
-        for i, book in enumerate(books_list):
-            if name in book:
-                index_to_remove = i
-                break
-      
-        if index_to_remove is not None:
-            del books_list[index_to_remove]
-            open("books.txt", "w").close() 
-            with open("books.txt", "a+") as f:
-                for book in books_list:
-                    f.write(book + "\n")
+        with open("books.txt", "r") as file:
+            books = file.read().splitlines()
+
+        new_books = []
+        removed = False
+
+        for book in books:
+            book_name = book.split(",")[0].strip()
+            if book_name.lower() != name.lower():
+                new_books.append(book)
+            else:
+                removed = True
+
+        with open("books.txt", "w") as file:
+            for book in new_books:
+                file.write(book + "\n")
+
+        if removed:
             print(f"Book '{name}' removed successfully.")
         else:
-            print(f"Book '{name}' not found.")    
-
-
-    def __del__(self):
-        self.file.close()    
-   
-def user_request () :
-    lib = Library()
-    while True :
-        request = input("***MENU*** \n 1)List Books \n 2)Add Book \n 3)Remove Book \n ")
-        if request == "1" :
-            lib.listBook() 
-        elif request == "2" :
-            lib.addBook() 
-        elif request == "3" :
-            lib.removeBook()
-        elif request == "Q" :
-            break
-        else :
-            print("Please write 1,2,3 or Q")
-
-user_request()
-
-
-
-
-
-
+            print(f"Book '{name}' not found.")
